97f594d9fe0117d6a80c81f56d9afca9e4b14329
HeatherCobas <Cobaruby40@gmail.com>
2024-10-29 11:57:50 -0400

    Add files via upload

---
+++ Ejercicio 15 (8)/Ejercicio15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace DecimoQuintoejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" By:Heather");
+            Console.WriteLine();
+
+            int N1, N2, N3;
+
+            Console.Write("Ingrese un primer número entero: ");
+            N1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese un segundo número entero: ");
+            N2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese un tercer número entero: ");
+            N3 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (N1 > N2 && N1 > N3)
+            {
+                Console.WriteLine("El número mayor es: " + N1);
+            }
+
+            else if (N2 < N1 && N2 > N3)
+            {
+                Console.WriteLine("El número mayor es: " + N2);
+            }
+
+            else
+            {
+                Console.WriteLine("El número mayor es: " + N3);
+            }
+
+        }
+    }
+}
+
