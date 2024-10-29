c6e93ec70e3783ad9ea4525677ca0393cf7896c4
HeatherCobas <Cobaruby40@gmail.com>
2024-10-29 11:37:51 -0400

    Add files via upload

---
+++ Ejercicio 2/Ejercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Segundo_ejercicio
+{internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo_de_Sueldo By:Heather");
+            Console.WriteLine();
+
+            double PH, HT, AFS, SFP, SB, SN, RD;
+
+            Console.WriteLine("Introduzca el Pago por hora");
+            PH = double.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca las Horas trabajadas");
+            HT = double.Parse(Console.ReadLine());
+
+            SB = PH * HT;
+            AFS = SB * 0.0287;
+            SFP = SB * 0.0304;
+
+            RD = AFS + SFP;
+            SN = SB - RD;
+
+            Console.WriteLine("El Sueldo Neto es de: " + SN);
+        }
+
+    }
+
+}
