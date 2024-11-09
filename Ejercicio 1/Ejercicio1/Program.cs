3d7d4d6bb1e002924d5f152286edf0f1914b6449
HeatherCobas <Cobaruby40@gmail.com>
2024-11-09 19:17:57 -0400

    Add files via upload

---
+++ Ejercicio 1/Ejercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Primer_ejercicio
+{
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calculadora By:Heather");
+        Console.WriteLine();
+
+        double Num1, Num2, Suma, Resta, Multiplicación, Divición;
+
+        Console.WriteLine("Introduce el primer valor: ");
+        Num1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Introduce el segundo valor: ");
+        Num2 = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        Suma = Num1 + Num2;
+        Resta = Num1 - Num2;
+        Multiplicación = Num1 * Num2 ;
+        Divición = Num1 / Num2;
+
+        Console.WriteLine("El resultado de la Suma es: " + Suma);
+        Console.WriteLine("El resultado de la Resta es: " + Resta);
+        Console.WriteLine("El resultado de la Multiplicación es: " + Multiplicación);
+        Console.WriteLine("El resultado de la Divición es: " + Divición);
+    }
+
+  } 
+
+}
---
+++ Ejercicio 8/Ejercicio8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Octavo_ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Disco By:Heather");
+            Console.WriteLine();
+
+            int Edad;
+            string Genero;
+
+         Console.WriteLine("Ingrese su edad");
+         Edad = Convert.ToInt32(Console.ReadLine());
+
+         Console.WriteLine("Ingrese su genero(Femenino/Masculino)");
+         Genero = Console.ReadLine();
+
+
+
+            if (Genero == "Femenino" && Edad >= 18)
+            { Console.WriteLine("Puede entrar gratis"); }
+
+            else if (Genero == "Femenino" && Edad < 18)
+            { Console.WriteLine("No puede pasar"); }
+
+            else if (Genero == "Masculino" && Edad >= 18)
+            {Console.WriteLine("Puede pasar pagando"); }
+
+            else{Console.WriteLine("No puede pasar");}
+
+        }
+    }
+}
