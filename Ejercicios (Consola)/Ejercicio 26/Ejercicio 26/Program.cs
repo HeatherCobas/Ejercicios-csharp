1b1f6bd460edd21d55c050f13b732168267001ab
HeatherCobas <Cobaruby40@gmail.com>
2024-11-18 01:27:40 -0400

    Add files via upload

---
+++ Ejercicios (Consola)/Ejercicio 26/Ejercicio 26/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Ejercicio_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Números impares 1 al 30 By:Heather");
+            Console.WriteLine();
+
+            for (int i = 1; i <= 30; i++)
+            {
+                if (i % 2 != 0)
+                { Console.WriteLine(i); }
+            }
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+
+        }
+    }
+}
