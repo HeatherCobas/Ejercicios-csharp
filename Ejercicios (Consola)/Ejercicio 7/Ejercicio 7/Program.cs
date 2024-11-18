0f803db993931e473eb4d47ea7e0ec8f416e7b27
HeatherCobas <Cobaruby40@gmail.com>
2024-11-18 01:11:57 -0400

    Add files via upload

---
+++ Ejercicios (Consola)/Ejercicio 7/Ejercicio 7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Ejercicio_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sueldo By:Heather");
+            Console.WriteLine();
+
+            double Sueldo, Aumento, T;
+
+            Console.WriteLine("Ingrese el sueldo");
+            Sueldo = double.Parse(Console.ReadLine());
+
+            Aumento = Sueldo * 0.15;
+            T = Sueldo + Aumento;
+
+            Console.WriteLine("El sueldo anterior es: " + Sueldo);
+            Console.WriteLine("El aumento es de: " + Aumento);
+            Console.WriteLine("El sueldo nuevo es de: " + T);
+        }
+    }
+}
