368f86f5907141b6fd872d6439c35581a14c7120
HeatherCobas <Cobaruby40@gmail.com>
2024-11-18 01:29:32 -0400

    Add files via upload

---
+++ Ejercicios (Consola)/Ejercicio 33/Ejercicio 33/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Ejercicio_33
+{
+    internal class Program
+    {
+        static void MostrarEstudiantes(string[] Nom, int[] Cali)
+        {
+
+            Console.WriteLine("Lista de estudiantes y sus calificaciones:");
+
+
+            for (int i = 0; i < Nom.Length; i++)
+            {
+
+                Console.WriteLine();
+                Console.WriteLine($"Estudiante: {Nom[i]}, Calificación: {Cali[i]}");
+
+            }
+        }
+
+        static void Main()
+        {
+            Console.Write("Ingrese la cantidad de estudiantes: ");
+            int Cant = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            string[] Nom = new string[Cant];
+            int[] Cali = new int[Cant];
+
+
+            for (int i = 0; i < Cant; i++)
+            {
+                Console.Write($"Ingrese el nombre del estudiante {i + 1}: ");
+                Nom[i] = Console.ReadLine();
+
+
+                Console.Write($"Ingrese la calificación de {Nom[i]}: ");
+                Cali[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            MostrarEstudiantes(Nom, Cali);
+
+            Console.ReadKey();
+        }
+    }
+}
