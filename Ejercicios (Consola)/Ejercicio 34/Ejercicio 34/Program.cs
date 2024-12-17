a1c4ce33e4b4be63a4d2c0a3f13f79dec8041b9c
HeatherCobas <Cobaruby40@gmail.com>
2024-12-17 13:39:51 -0400

    Add files via upload

---
+++ Ejercicios (Consola)/Ejercicio 34/Ejercicio 34/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Ejercicio_34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de números By:Heather");
+            Console.WriteLine();
+
+            Console.WriteLine("Introduce 5 números:");
+
+            int[] numeros = new int[5];
+            int suma = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Número {i + 1}: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+                suma += numeros[i];
+            }
+
+            Console.WriteLine();
+
+
+            if (suma == 100)
+            { Console.WriteLine($"La suma de los números es: {suma}. igual a 100."); }
+
+            else if (suma > 100)
+            { Console.WriteLine($"La suma de los números es: {suma}. Es mayor."); }
+
+            else
+            { Console.WriteLine($"La suma de los números es: {suma}. Es menor a 100."); }
+
+
+            Console.ReadKey();
+        }
+    }
+}
