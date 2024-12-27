bea39709941b9e0529cc3bccf37741e4f73d3732
HeatherCobas <Cobaruby40@gmail.com>
2024-12-27 15:21:18 -0400

    Add files via upload

---
+++ Ejercicios (Interfaces)/Ejercicio 4 - Desafío 2/Ejercicio 4 - Desafío 2/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace Ejercicio_4___Desafío_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCurso = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.lblSección = new System.Windows.Forms.Label();
+            this.lblÁrea = new System.Windows.Forms.Label();
+            this.lblN1 = new System.Windows.Forms.Label();
+            this.lblN2 = new System.Windows.Forms.Label();
+            this.lblN3 = new System.Windows.Forms.Label();
+            this.lblN4 = new System.Windows.Forms.Label();
+            this.lblPromedio = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCurso = new System.Windows.Forms.TextBox();
+            this.txtSección = new System.Windows.Forms.TextBox();
+            this.txtÁrea = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.txtN4 = new System.Windows.Forms.TextBox();
+            this.txtN3 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(32, 50);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(65, 20);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblCurso
+            // 
+            this.lblCurso.AutoSize = true;
+            this.lblCurso.Location = new System.Drawing.Point(32, 91);
+            this.lblCurso.Name = "lblCurso";
+            this.lblCurso.Size = new System.Drawing.Size(51, 20);
+            this.lblCurso.TabIndex = 1;
+            this.lblCurso.Text = "Curso";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(32, 130);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(45, 20);
+            this.lblSexo.TabIndex = 2;
+            this.lblSexo.Text = "Sexo";
+            // 
+            // lblSección
+            // 
+            this.lblSección.AutoSize = true;
+            this.lblSección.Location = new System.Drawing.Point(254, 52);
+            this.lblSección.Name = "lblSección";
+            this.lblSección.Size = new System.Drawing.Size(66, 20);
+            this.lblSección.TabIndex = 3;
+            this.lblSección.Text = "Sección";
+            // 
+            // lblÁrea
+            // 
+            this.lblÁrea.AutoSize = true;
+            this.lblÁrea.Location = new System.Drawing.Point(254, 91);
+            this.lblÁrea.Name = "lblÁrea";
+            this.lblÁrea.Size = new System.Drawing.Size(43, 20);
+            this.lblÁrea.TabIndex = 4;
+            this.lblÁrea.Text = "Área";
+            // 
+            // lblN1
+            // 
+            this.lblN1.AutoSize = true;
+            this.lblN1.Location = new System.Drawing.Point(10, 47);
+            this.lblN1.Name = "lblN1";
+            this.lblN1.Size = new System.Drawing.Size(29, 20);
+            this.lblN1.TabIndex = 5;
+            this.lblN1.Text = "N1";
+            // 
+            // lblN2
+            // 
+            this.lblN2.AutoSize = true;
+            this.lblN2.Location = new System.Drawing.Point(10, 86);
+            this.lblN2.Name = "lblN2";
+            this.lblN2.Size = new System.Drawing.Size(29, 20);
+            this.lblN2.TabIndex = 6;
+            this.lblN2.Text = "N2";
+            // 
+            // lblN3
+            // 
+            this.lblN3.AutoSize = true;
+            this.lblN3.Location = new System.Drawing.Point(10, 124);
+            this.lblN3.Name = "lblN3";
+            this.lblN3.Size = new System.Drawing.Size(29, 20);
+            this.lblN3.TabIndex = 7;
+            this.lblN3.Text = "N3";
+            this.lblN3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblN4
+            // 
+            this.lblN4.AutoSize = true;
+            this.lblN4.Location = new System.Drawing.Point(259, 50);
+            this.lblN4.Name = "lblN4";
+            this.lblN4.Size = new System.Drawing.Size(29, 20);
+            this.lblN4.TabIndex = 8;
+            this.lblN4.Text = "N4";
+            // 
+            // lblPromedio
+            // 
+            this.lblPromedio.AutoSize = true;
+            this.lblPromedio.Location = new System.Drawing.Point(259, 89);
+            this.lblPromedio.Name = "lblPromedio";
+            this.lblPromedio.Size = new System.Drawing.Size(19, 20);
+            this.lblPromedio.TabIndex = 9;
+            this.lblPromedio.Text = "P";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(259, 127);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(56, 20);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.Text = "Status";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(113, 44);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 26);
+            this.txtNombre.TabIndex = 11;
+            // 
+            // txtCurso
+            // 
+            this.txtCurso.Location = new System.Drawing.Point(113, 85);
+            this.txtCurso.Name = "txtCurso";
+            this.txtCurso.Size = new System.Drawing.Size(100, 26);
+            this.txtCurso.TabIndex = 12;
+            // 
+            // txtSección
+            // 
+            this.txtSección.Location = new System.Drawing.Point(340, 46);
+            this.txtSección.Name = "txtSección";
+            this.txtSección.Size = new System.Drawing.Size(100, 26);
+            this.txtSección.TabIndex = 13;
+            // 
+            // txtÁrea
+            // 
+            this.txtÁrea.Location = new System.Drawing.Point(340, 85);
+            this.txtÁrea.Name = "txtÁrea";
+            this.txtÁrea.Size = new System.Drawing.Size(100, 26);
+            this.txtÁrea.TabIndex = 14;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLimpiar.Location = new System.Drawing.Point(348, 444);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnLimpiar.Size = new System.Drawing.Size(99, 42);
+            this.btnLimpiar.TabIndex = 15;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCalcular.Location = new System.Drawing.Point(453, 444);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(99, 42);
+            this.btnCalcular.TabIndex = 16;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Red;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSalir.Location = new System.Drawing.Point(558, 444);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(99, 42);
+            this.btnSalir.TabIndex = 17;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.txtÁrea);
+            this.groupBox1.Controls.Add(this.lblNombre);
+            this.groupBox1.Controls.Add(this.lblCurso);
+            this.groupBox1.Controls.Add(this.lblSexo);
+            this.groupBox1.Controls.Add(this.lblSección);
+            this.groupBox1.Controls.Add(this.txtSección);
+            this.groupBox1.Controls.Add(this.lblÁrea);
+            this.groupBox1.Controls.Add(this.txtCurso);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Location = new System.Drawing.Point(30, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(478, 182);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos_Personales";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(258, 130);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(44, 24);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "F";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(113, 130);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 24);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "M";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtStatus);
+            this.groupBox2.Controls.Add(this.txtPromedio);
+            this.groupBox2.Controls.Add(this.lblN1);
+            this.groupBox2.Controls.Add(this.txtN4);
+            this.groupBox2.Controls.Add(this.lblN2);
+            this.groupBox2.Controls.Add(this.txtN3);
+            this.groupBox2.Controls.Add(this.lblN3);
+            this.groupBox2.Controls.Add(this.txtN2);
+            this.groupBox2.Controls.Add(this.lblN4);
+            this.groupBox2.Controls.Add(this.txtN1);
+            this.groupBox2.Controls.Add(this.lblPromedio);
+            this.groupBox2.Controls.Add(this.lblStatus);
+            this.groupBox2.Location = new System.Drawing.Point(32, 249);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(476, 176);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Calificaciones";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(354, 127);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(100, 26);
+            this.txtStatus.TabIndex = 25;
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(354, 89);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(100, 26);
+            this.txtPromedio.TabIndex = 24;
+            // 
+            // txtN4
+            // 
+            this.txtN4.Location = new System.Drawing.Point(354, 50);
+            this.txtN4.Name = "txtN4";
+            this.txtN4.Size = new System.Drawing.Size(100, 26);
+            this.txtN4.TabIndex = 23;
+            // 
+            // txtN3
+            // 
+            this.txtN3.Location = new System.Drawing.Point(91, 124);
+            this.txtN3.Name = "txtN3";
+            this.txtN3.Size = new System.Drawing.Size(100, 26);
+            this.txtN3.TabIndex = 22;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(91, 86);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 26);
+            this.txtN2.TabIndex = 21;
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(91, 47);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 26);
+            this.txtN1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 498);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpiar);
+            this.Name = "Form1";
+            this.Text = "Registro Calificaciones";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCurso;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label lblSección;
+        private System.Windows.Forms.Label lblÁrea;
+        private System.Windows.Forms.Label lblN1;
+        private System.Windows.Forms.Label lblN2;
+        private System.Windows.Forms.Label lblN3;
+        private System.Windows.Forms.Label lblN4;
+        private System.Windows.Forms.Label lblPromedio;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCurso;
+        private System.Windows.Forms.TextBox txtSección;
+        private System.Windows.Forms.TextBox txtÁrea;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.TextBox txtN4;
+        private System.Windows.Forms.TextBox txtN3;
+    }
+}
+
---
+++ Ejercicios (Interfaces)/Ejercicio 4 - Desafío 2/Ejercicio 4 - Desafío 2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Ejercicio_4___Desafío_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNombre.Clear();
+            txtCurso.Clear();
+            txtSección.Clear();
+            txtÁrea.Clear();
+            txtPromedio.Clear();
+            txtN1.Clear();
+            txtN2.Clear();
+            txtN3.Clear();
+            txtN4.Clear();
+            txtStatus.Clear();
+            txtStatus.BackColor = SystemColors.Window;
+            txtStatus.ForeColor = SystemColors.WindowText;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtN1.Text);
+            double n2 = Convert.ToDouble(txtN2.Text);
+            double n3 = Convert.ToDouble(txtN3.Text);
+            double n4 = Convert.ToDouble(txtN4.Text);
+
+            double promedio = (n1 + n2 + n3 + n4) / 4;
+
+            txtPromedio.Text = promedio.ToString("F2");
+
+            if (promedio >= 70)
+            {
+                txtStatus.Text = "Aprobado";
+                txtStatus.BackColor = Color.Green;
+                txtStatus.ForeColor = Color.White;
+            }
+            else
+            {
+                txtStatus.Text = "Reprobado";
+                txtStatus.BackColor = Color.Red;
+                txtStatus.ForeColor = Color.White;
+            }
+
+        }
+
+        
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
