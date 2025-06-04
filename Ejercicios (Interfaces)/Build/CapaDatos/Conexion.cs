ff3839520e9b070b6b5c16422e9c6dc3944f618b
HeatherCobas <Cobaruby40@gmail.com>
2025-06-04 13:27:22 -0400

    Add files via upload

---
+++ Ejercicios (Interfaces)/Build/CapaDatos/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        private readonly string cadenaConexion = "Server=Pita;Database=DBSistemaLogin;Integrated Security=True;";
+
+        public SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(cadenaConexion);
+        }
+    }
+}
